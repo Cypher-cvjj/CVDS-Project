09d1a8de1e42d63a5030c2c84fdb44531720f98a
unknown <laura.alvarado-g@labinfo.is.escuelaing.edu.co>
2022-04-23 09:44:13 -0500

    base de datos

--- Tables.sql
+++ Tables.sql
@@ -6,7 +6,8 @@ nombre VARCHAR(20) NOT NULL,
 contrasena VARCHAR(16) NOT NULL,
 correo VARCHAR(20) NOT NULL,
 telefono VARCHAR(20),
-tipousuario INT NOT NULL
+tipousuario INT NOT null,
+reserva int not null
 );
 
 CREATE TABLE recursos(
@@ -15,39 +16,63 @@ id_recursos INT NOT NULL,
 nombre VARCHAR(20) NOT NULL,
 ubicacion VARCHAR(20) NOT NULL,
 capacidad INT NOT NULL,
-tiporecurso VARCHAR(20) NOT NULL,
-horario INT NOT NULL
+disponibilidad BOOLEAN NOT null,
+horario_inicial TIME not null,
+horario_final TIME not null,
+tiporecurso VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE horarios(
-
-id_horario INT NOT NULL,
-fecha DATE NOT NULL,
-horario_inicial TIME NOT NULL,
-horario_final TIME NOT NULL,
-disponibilidad BOOLEAN NOT NULL
+create table tiporecurso(
+idtipo VARCHAR(20) not null,
+nombre VARCHAR(20) not NULL
 );
+
+create table ubicacion(
+idubicacion VARCHAR(20) not null,
+nombre VARCHAR(20) not NULL
+);
+
 
 CREATE TABLE TipoUsuario(
 id INT not null,
 nombre VARCHAR(10) not null
 );
 
+create table reserva(
+id_reserva INT not null,
+nombre VARCHAR(20) not null,
+recurso INT not null,
+fechasoli Date not null,
+fechaini Date not null,
+fechafin Date not null
+);
 
 --PK
 ALTER TABLE usuarios ADD CONSTRAINT pk_usuario PRIMARY KEY (documento);
 ALTER TABLE recursos ADD CONSTRAINT pk_recursos PRIMARY KEY (id_recursos);
 ALTER TABLE TipoUsuario  ADD CONSTRAINT pk_tipousuario PRIMARY KEY (id);
+ALTER TABLE Tiporecurso ADD CONSTRAINT pk_tiporecurso PRIMARY KEY (idtipo);
+ALTER TABLE ubicacion  ADD CONSTRAINT pk_tipoubicacion PRIMARY KEY (idubicacion);
+ALTER TABLE reserva  ADD CONSTRAINT pk_reserva PRIMARY KEY (id_reserva);
 
 
 --UK
 ALTER TABLE usuarios ADD CONSTRAINT uk_correo UNIQUE (correo);
 
 --FK
-ALTER TABLE usuarios  
+ALTER TABLE usuarios
 ADD FOREIGN KEY (tipousuario) REFERENCES tipousuario (id);
-ALTER TABLE recursos  
-ADD FOREIGN KEY (horario) REFERENCES horarios (id_horario);
+ALTER TABLE usuarios
+ADD FOREIGN KEY (reserva) REFERENCES reserva (id_reserva);
+ALTER TABLE recursos
+ADD FOREIGN KEY (ubicacion) REFERENCES ubicacion (idubicacion);
+ALTER TABLE recursos
+ADD FOREIGN KEY (tiporecurso) REFERENCES tiporecurso (idtipo);
+ALTER TABLE reserva
+ADD FOREIGN KEY (recurso) REFERENCES recursos (id_recursos);
+
+
+
 
 --POBLAR
 INSERT INTO recursos (id_recursos,nombre,ubicacion,capacidad,tiporecurso,fecha,horario_inicial,horario_final)
@@ -62,10 +87,13 @@ VALUES (2,'libro2','biblioteca2',1,'libro','2020-11-11','07:00:00','10:00:00');
 INSERT INTO recursos (id_recursos,nombre,ubicacion,capacidad,tiporecurso,fecha,horario_inicial,horario_final)
 VALUES (4,'sala','bibliotecaa1',8,'sala','2020-11-11','13:00:00','15:00:00');
 
---DROP 
+--DROP
 DROP TABLE usuarios;
 DROP TABLE recursos;
 DROP TABLE tipousuario;
+DROP TABLE ubicacion;
+DROP TABLE tiporecurso;
+drop table reserva;
 
 --DELETE 
 Delete from recursos;
