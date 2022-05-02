a4e0d9e1ac5da35a459f3c4c2fcab22bf054c6bd
Camilo Archila <esteban.archila@mail.escuelaing.edu.co>
2022-05-01 22:25:31 -0500

    bean y mapper reserva para guardar fecha C

--- src/main/java/edu/eci/cvds/view/ReservaBean.java
+++ src/main/java/edu/eci/cvds/view/ReservaBean.java
@@ -6,6 +6,7 @@ import edu.eci.cvds.services.ECILibraryServices;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import java.util.Date;
 
 @ManagedBean(name = "ReservaBean")
 @RequestScoped
@@ -15,6 +16,54 @@ public class ReservaBean extends BasePageBean {
     @Inject
     private ECILibraryServices eciLibraryServices;
 
+    private int id_recurso;
+    private String nombre_recurso;
+    private Date fechainicialreserva;
+    private Date fechafinalreserva;
 
+
+    public Date getFechainicialreserva() {
+        return fechainicialreserva;
+    }
+
+    public Date getFechafinalreserva() {
+        return fechafinalreserva;
+    }
+
+    public void setFechainicialreserva(Date fechainicialreserva) {
+        this.fechainicialreserva = fechainicialreserva;
+        System.out.println(fechainicialreserva);
+    }
+
+    public void setFechafinalreserva(Date fechafinalreserva) {
+        this.fechafinalreserva = fechafinalreserva;
+        System.out.println(fechafinalreserva);
+    }
+
+    public ECILibraryServices getEciLibraryServices() {
+        return eciLibraryServices;
+    }
+
+    public int getId_recurso() {
+        return id_recurso;
+    }
+
+    public void setEciLibraryServices(ECILibraryServices eciLibraryServices) {
+        this.eciLibraryServices = eciLibraryServices;
+    }
+
+    public void setId_recurso(int id_recurso) {
+        this.id_recurso = id_recurso;
+    }
+
+    public void setNombre_recurso(String nombre_recurso) {
+        this.nombre_recurso = nombre_recurso;
+    }
+
+
+
+    public String getNombre_recurso() {
+        return nombre_recurso;
+    }
 
 }
